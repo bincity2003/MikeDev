8991f629254946535daede8f9be1c28fcfe55baa
Binh Nguyen <bincity2003@outlook.com>
2019-06-29 19:30:24 +0700

    Added TestA for DbTable

---
+++ MikeDev.Test/DbTableTest.cs
@@ -0,0 +1,25 @@
+﻿using MikeDev.DB;
+using NUnit.Framework;
+
+namespace MikeDev.Test
+{
+    [TestFixture]
+    class DbTableTest
+    {
+        /// <summary>
+        /// This test requires DbTable to initialize correctly.
+        /// </summary>
+        [Test]
+        public void TestA()
+        {
+            DbTable obj = new DbTable(new string[]
+            {
+                "Name",
+                "Age"
+            });
+            Assert.IsTrue(obj.FieldNames == new string[] { "Name", "Age" });
+            Assert.IsFalse(obj.FieldNames == new string[] { "Name", "age" });
+            Assert.IsTrue(obj.Count == 2);
+        }
+    }
+}
