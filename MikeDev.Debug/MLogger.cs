e5e1658d765b4558fecc6ff1c8336913539c22f4
bincity2003 <bincity2003@gmail.com>
2019-06-30 20:28:43 +0700

    Add MLogger class

---
+++ MikeDev.Debug/MLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MikeDev.Debug
+{
+    /// <summary>
+    /// MLogger class is used for debugging and logging procedures.
+    /// </summary>
+    public class MLogger
+    {
+        
+    }
+}
