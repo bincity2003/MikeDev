84950227e3d9e61c5ed3f0c898daad456e65087f
bincity2003 <bincity2003@gmail.com>
2019-06-30 20:45:43 +0700

    Add Log method and documentation

--- MikeDev.Debug/MLogger.cs
+++ MikeDev.Debug/MLogger.cs
@@ -9,6 +9,51 @@ namespace MikeDev.Debug
     /// </summary>
     public class MLogger
     {
-        
+        private bool _DefaultCulture;
+
+        /// <summary>
+        /// Write message to captured file.
+        /// </summary>
+        /// <param name="level">The level of severity.</param>
+        /// <param name="message">The message to write.</param>
+        /// <param name="includeTime">Whether include time or not.</param>
+        /// <returns>True if log is written successfully. Otherwise, false.</returns>
+        public bool Log(string message, bool includeTime = true, LogLevel level = LogLevel.Info)
+        {
+            string Data = _InternalPrepareMessage(message, includeTime, level);
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// LogLevel enum indicates the severity of the message.
+        /// </summary>
+        public enum LogLevel
+        {
+            Info,
+            Warning,
+            Error
+        }
+
+        /// <summary>
+        /// Create message based on default template.
+        /// </summary>
+        private string _InternalPrepareMessage(string message, bool includeTime, LogLevel level)
+        {
+            if (_DefaultCulture)
+            {
+                // Add timestamp
+                string Message = includeTime ? $"[{DateTime.Now}]" : "";
+
+                // Add severity
+                Message += $"{level.ToString()} : ";
+
+                // Add message
+                Message += message;
+
+                return Message;
+            }
+
+            throw new NotImplementedException();
+        }
     }
 }
