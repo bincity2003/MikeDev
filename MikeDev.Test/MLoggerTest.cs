cab6dfdd75262a83ab06d678410ed5c8bb31fea1
bincity2003 <bincity2003@gmail.com>
2019-07-04 07:39:57 +0700

    Add MLoggerTest

---
+++ MikeDev.Test/MLoggerTest.cs
@@ -0,0 +1,18 @@
+﻿using MikeDev.Debug;
+using NUnit.Framework;
+
+namespace MikeDev.Test
+{
+    [TestFixture]
+    internal class MLoggerTest
+    {
+        /// <summary>
+        /// This test requires MLogger to correctly process message.
+        /// </summary>
+        [Test]
+        public void TestA()
+        {
+
+        }
+    }
+}
