ed10030c49c5f8dd8cdc4a9a8b2c7e650d257e25
bincity2003 <bincity2003@gmail.com>
2019-07-04 07:54:47 +0700

    Add TestB for MLogger

--- MikeDev.Test/MLoggerTest.cs
+++ MikeDev.Test/MLoggerTest.cs
@@ -14,8 +14,21 @@ namespace MikeDev.Test
         {
             MLogger obj = new MLogger("C:\\Users\\Thanh Cong\\Desktop\\log.txt");
             string Message = "This is a test message!";
+
             string Result = obj.ProcessMessage(Message, false);
             Assert.AreEqual(Result, "Info : This is a test message!\n");
+
+            Result = obj.ProcessMessage(Message, false, MLogger.LogLevel.Warning);
+            Assert.AreEqual(Result, "Warning : This is a test message!\n");
+        }
+
+        public void TestB()
+        {
+            MLogger obj = new MLogger("C:\\Users\\Thanh Cong\\Desktop\\log.txt");
+            string Message = "This is a test message!";
+
+            bool Flag = obj.Log(Message);
+            Assert.IsTrue(Flag);
         }
     }
 }
