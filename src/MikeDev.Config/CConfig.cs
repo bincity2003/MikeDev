29a82ffc09280ff35be029e83158c05e869d2a3f
bincity2003 <bincity2003@gmail.com>
2019-07-17 18:02:43 +0700

    Add first 4/5 APIs requirement

--- src/MikeDev.Config/CConfig.cs
+++ src/MikeDev.Config/CConfig.cs
@@ -1,8 +1,67 @@
 ﻿using System;
+using System.Xml.Linq;
 
 namespace MikeDev.Config
 {
     public class CConfig
     {
+        XElement _Storage;
+
+        /// <summary>
+        /// Create new empty config.
+        /// </summary>
+        public CConfig()
+        {
+            _Storage = new XElement("configuration");
+        }
+
+        /// <summary>
+        /// Import existing CConfig from file.
+        /// </summary>
+        /// <param name="cconfigFile">Path to CConfig file.</param>
+        public CConfig(string cconfigFile)
+        {
+            _Storage = XElement.Load(cconfigFile);
+
+            if (_Storage.Name.LocalName != "configuration")
+            {
+                throw new Exception("Not a configuration file!");
+            }
+        }
+
+        /// <summary>
+        /// Try parse XElement to a CConfig.
+        /// </summary>
+        /// <param name="config">Existing XElement config.</param>
+        public CConfig(XElement config)
+        {
+            _Storage = config;
+        }
+
+        /// <summary>
+        /// Get the value of an attribute.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public string this[string name] => _Storage.Element(name).Value;
+
+        /// <summary>
+        /// Export CConfig to an XML-string.
+        /// </summary>
+        /// <returns></returns>
+        public string Export()
+        {
+            return _Storage.ToString();
+        }
+
+        /// <summary>
+        /// Export CConfig to file.
+        /// </summary>
+        /// <param name="cconfigFile">Path to CConfig file. Will be overwritten.</param>
+        public void Export(string cconfigFile)
+        {
+            System.IO.File.Create(cconfigFile);
+            _Storage.Save(cconfigFile);
+        }
     }
 }
