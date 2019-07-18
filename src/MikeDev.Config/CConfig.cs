3206c2fa1667175699ebd41fc0717cfff5b91047
bincity2003 <bincity2003@gmail.com>
2019-07-18 18:26:50 +0700

    Remove unnecessary Read method

--- src/MikeDev.Config/CConfig.cs
+++ src/MikeDev.Config/CConfig.cs
@@ -113,10 +113,5 @@ namespace MikeDev.Config
                 throw new ArgumentException("Name not exists!");
             }
         }
-
-        public string Read(string name)
-        {
-            return _Names.Contains(name) ? _Storage.Element(name).Value : throw new ArgumentException("Name not exists!");
-        }
     }
 }
