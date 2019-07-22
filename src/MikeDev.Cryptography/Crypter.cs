ca73326baf6efc8dcb8cb11ba3b1534151ba8a0d
bincity2003 <bincity2003@gmail.com>
2019-07-22 13:45:20 +0700

    Add full ComputeHash suite

--- src/MikeDev.Cryptography/Crypter.cs
+++ src/MikeDev.Cryptography/Crypter.cs
@@ -9,6 +9,49 @@ namespace MikeDev.Cryptography
     /// </summary>
     public static class Crypter
     {
-        
+        /// <summary>
+        /// Compute hash of a System.Byte[] object.
+        /// </summary>
+        /// <param name="s">Byte array to be hashed.</param>
+        public static string ComputeHash(byte[] byteArray)
+        {
+            return _InternalComputeHash(byteArray);
+        }      
+
+        /// <summary>
+        /// Compute hash of a System.String object.
+        /// </summary>
+        /// <param name="s">String to be hashed.</param>
+        public static string ComputeHash(string s)
+        {
+            if (s is null)
+            {
+                throw new ArgumentNullException(nameof(s));
+            }
+
+            return ComputeHash(Encoding.UTF8.GetBytes(s));
+        }
+
+        /// <summary>
+        /// Compute hash of a System.Object object.
+        /// </summary>
+        /// <param name="obj">Object to be hashed.</param>
+        public static string ComputeHash(object obj)
+        {
+            if (obj is null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            return ComputeHash(obj.ToString());
+        }
+
+        /// <summary>
+        /// Compute hash with SHA512 algorithm. Internal.
+        /// </summary>
+        private static string _InternalComputeHash(byte[] byteArray)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
