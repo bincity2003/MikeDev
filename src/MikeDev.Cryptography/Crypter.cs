76b4051b1e39dc00eebaefc2e6a19a445e072a27
bincity2003 <bincity2003@gmail.com>
2019-07-22 12:17:00 +0700

    Add Crypter class template

---
+++ src/MikeDev.Cryptography/Crypter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MikeDev.Cryptography
+{
+    /// <summary>
+    /// Crypter exposes many APIs related to cryptography.
+    /// </summary>
+    public static class Crypter
+    {
+        
+    }
+}
