cfc3c4ed7c8addc91b5502d326081bf9521bfe7e
bincity2003 <bincity2003@gmail.com>
2019-07-23 07:47:09 +0700

    Add CrypterTest template

--- src/MikeDev.Test/CrypterTest.cs
+++ src/MikeDev.Test/CrypterTest.cs
@@ -6,6 +6,31 @@ namespace MikeDev.Test
     [TestFixture]
     internal class CrypterTest
     {
+        /// <summary>
+        /// This test requires Crypter to correctly compute hash of a string.
+        /// </summary>
+        [Test]
+        public void TestA()
+        {
 
+        }
+
+        /// <summary>
+        /// This test requires Crypter to correctly compute hash of a byte array.
+        /// </summary>
+        [Test]
+        public void TestB()
+        {
+
+        }
+
+        /// <summary>
+        /// This test requires Crypter to correcly encrypt/decrypt a byte array.
+        /// </summary>
+        [Test]
+        public void TestC()
+        {
+
+        }
     }
 }
